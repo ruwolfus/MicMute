10a3ff2fc748e6c9643d453deb1944da91ba74fe
mist.poryvaev <mist.poryvaev@81dcea23-720b-4ff1-bc4e-8d28d5d6887b>
2010-06-28 14:49:11 +0000

    0.1.2

--- mic_mute/key_hook/key_hook.cpp
+++ mic_mute/key_hook/key_hook.cpp
@@ -8,26 +8,62 @@
 #pragma managed(push, off)
 #endif
 
-HANDLE Event = 0;
-WPARAM PrevCode = 0;
+static HANDLE Event = 0;
+
+struct KeyHook_Struct
+{
+	WPARAM PrevCode;
+	int keys_count;
+	int key1;
+	int key2;
+	BOOL enabled;
+};
+
+KeyHook_Struct * kstruct;
+HANDLE map_handle;
 
 extern "C"
 {
 	__declspec(dllexport) LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 	{
-		if ((nCode >= 0) && ((lParam & (1 << 30)) == (1 << 30)))
+		if (kstruct->enabled && (nCode >= 0) && ((lParam & (1 << 30)) == 0))
 		{
-			if ((PrevCode == VK_RWIN) && (wParam == VK_CONTROL)) 
+			if (((kstruct->PrevCode == kstruct->key1) && (wParam == kstruct->key2) && (kstruct->keys_count == 2)) || ((wParam == kstruct->key1) && (kstruct->keys_count == 1))) 
 			{
 				SetEvent(Event);
-				PrevCode = 0;
+				kstruct->PrevCode = 0;
 			}
 			else
 			{
-				PrevCode = wParam;
+				kstruct->PrevCode = wParam;
 			}
 		}
 		return CallNextHookEx(0, nCode, wParam,lParam);
+	}
+
+	__declspec(dllexport) void __stdcall SetShortcut(int _count, int _key1, int _key2)
+	{
+		if (_count > 2)
+		{
+			_count = 2;
+		}
+		if (_count < 1)
+		{
+			_count = 1;
+		}
+		kstruct->keys_count = _count;
+		kstruct->key1 = _key1;
+		kstruct->key2 = _key2; 
+	}
+	__declspec(dllexport) void __stdcall GetShortcut(int * _count, int * _key1, int * _key2)
+	{
+		* _count = kstruct->keys_count;
+		* _key1 = kstruct->key1;
+		* _key2 = kstruct->key2;
+	}
+	__declspec(dllexport) void __stdcall SetEnabled(bool _state)
+	{
+		kstruct->enabled = _state;
 	}
 };
 
@@ -39,9 +75,25 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 	if (ul_reason_for_call == DLL_PROCESS_ATTACH)
 	{
 		Event = CreateEvent(NULL, TRUE, FALSE, _T("Hooked!"));
+//		MessageBox(0, _T("key_hook.dll attached"), _T("message"), MB_OK);
+		map_handle = CreateFileMapping(
+			INVALID_HANDLE_VALUE,    // use paging file
+			NULL,                    // default security 
+			PAGE_READWRITE,          // read/write access
+			0,                       // max. object size 
+			sizeof(KeyHook_Struct),                // buffer size  
+			_T("KeyHook_Struct!"));                 // name of mapping object
+
+		kstruct = (KeyHook_Struct *)MapViewOfFile(map_handle,   // handle to map object
+			FILE_MAP_ALL_ACCESS, // read/write permission
+			0,                   
+			0,                   
+			sizeof(KeyHook_Struct));   
 	}
 	if (ul_reason_for_call == DLL_PROCESS_DETACH)
 	{
+		UnmapViewOfFile(kstruct);
+		CloseHandle(map_handle);
 		CloseHandle(Event);
 	}
     return TRUE;
--- mic_mute/mic_mute/mic_mute.cpp
+++ mic_mute/mic_mute/mic_mute.cpp
@@ -4,6 +4,14 @@
 #include "stdafx.h"
 #include "mic_mute.h"
 #include "ShellAPI.h"
+#include "./../key_hook/key_hook.h"
+#include "strsafe.h"
+
+_SetShortCut	SetShortcut;
+_SetEnabled		SetEnabled;
+_GetShortCut	GetShortcut;
+
+TCHAR _ini[] = _T(".\\mic_mute.ini");
 
 #define MAX_LOADSTRING 100
 
@@ -12,23 +20,26 @@ HINSTANCE hInst;								// current instance
 TCHAR szTitle[MAX_LOADSTRING];					// The title bar text
 TCHAR szWindowClass[MAX_LOADSTRING];			// the main window class name
 DWORD SavedVolume = 0;
-BOOL SavedMicState, SavedLineState;
+UINT TrayMsg;
+HMENU TrayMenu;
 
 // Forward declarations of functions included in this code module:
 ATOM				MyRegisterClass(HINSTANCE hInstance);
 BOOL				InitInstance(HINSTANCE, int);
 LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
-VOID				Mute(HWND hWnd);
+INT_PTR CALLBACK	SetupShortcut(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK	ShortcutEditProc(HWND, UINT, WPARAM, LPARAM);
+VOID				MuteToggle(HWND hWnd);
+VOID				StartMutedToggle(HWND hWnd);
 
 CMixer mixer_mic_in(MIXERLINE_COMPONENTTYPE_SRC_MICROPHONE, CMixer::Record);
-CMixer mixer_mic_out(MIXERLINE_COMPONENTTYPE_SRC_MICROPHONE, CMixer::Play);
-CMixer mixer_line_out(MIXERLINE_COMPONENTTYPE_SRC_LINE, CMixer::Play);
 
 HANDLE HookEvent = 0;
 HANDLE StopEvent = 0;
 HANDLE Thread = 0;
 HWND AppHWnd = 0;
+BOOL StartMuted = FALSE;
 
 DWORD WINAPI ThreadProc( LPVOID lpParam ) 
 {
@@ -37,7 +48,7 @@ DWORD WINAPI ThreadProc( LPVOID lpParam )
 		if (WaitForSingleObject(HookEvent, 0) == WAIT_OBJECT_0)
 		{
 			ResetEvent(HookEvent);
-			Mute(HWND(lpParam));
+			MuteToggle(HWND(lpParam));
 		}
 		if (WaitForSingleObject(StopEvent, 0) == WAIT_OBJECT_0)
 		{
@@ -63,13 +74,27 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 	static HINSTANCE hinstDLL; 
 	static HHOOK hhook; 
 
-	SavedMicState = mixer_mic_out.GetMute();
-	SavedLineState = mixer_line_out.GetMute();
-
 	hinstDLL = LoadLibrary(_T("key_hook.dll")); 
 	hkprc = (HOOKPROC)GetProcAddress(hinstDLL, "_KeyboardProc@12"); 
+	SetShortcut = (_SetShortCut)GetProcAddress(hinstDLL, "_SetShortcut@12");
+	GetShortcut = (_GetShortCut)GetProcAddress(hinstDLL, "_GetShortcut@12");
+	SetEnabled = (_SetEnabled)GetProcAddress(hinstDLL, "_SetEnabled@4");
 	hhook = SetWindowsHookEx(WH_KEYBOARD,hkprc,hinstDLL,NULL); 
 	HookEvent = CreateEvent(NULL, TRUE, FALSE, _T("Hooked!"));
+
+	TCHAR _str[1024];
+	int _count, _key1, _key2, _start_muted;
+	GetPrivateProfileString(_T("Mic_Mute"), _T("ShortCut_Count"), _T("2"), _str, 1024, _ini);
+	_stscanf(_str, _T("%i"), &_count);
+	GetPrivateProfileString(_T("Mic_Mute"), _T("ShortCut_Key1"), _T("92"), _str, 1024, _ini);
+	_stscanf(_str, _T("%i"), &_key1);
+	GetPrivateProfileString(_T("Mic_Mute"), _T("ShortCut_Key2"), _T("17"), _str, 1024, _ini);
+	_stscanf(_str, _T("%i"), &_key2);
+	GetPrivateProfileString(_T("Mic_Mute"), _T("StartMuted"), _T("0"), _str, 1024, _ini);
+	_stscanf(_str, _T("%i"), &_start_muted);
+	StartMuted = (_start_muted != 0);
+
+	SetShortcut(_count, _key1, _key2);
 
 	// Initialize global strings
 	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
@@ -77,7 +102,7 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 	MyRegisterClass(hInstance);
 
 	// Perform application initialization:
-	if (!InitInstance (hInstance, SW_MINIMIZE))
+	if (!InitInstance (hInstance, SW_HIDE))
 	{
 		return FALSE;
 	}
@@ -91,17 +116,35 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 		0);   // returns the thread identifier 
 	StopEvent = CreateEvent(NULL, TRUE, FALSE, _T("Stop!"));
 
+	TrayMsg = RegisterWindowMessage(_T("Tray!"));
+
 	NOTIFYICONDATA nid;
 	nid.cbSize = sizeof(nid);
 	nid.hWnd = AppHWnd;
 	nid.uID = 1;
 	nid.hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_MIC_MUTE));
-	nid.uFlags = NIF_ICON | NIF_TIP | NIF_MESSAGE;
+	nid.uFlags = NIF_ICON | NIF_TIP | NIF_MESSAGE | NIF_INFO;
+	nid.uCallbackMessage = TrayMsg;
+	nid.uTimeout = 5000;
+	nid.dwInfoFlags = NIIF_USER | NIIF_NOSOUND;
 	TCHAR _tip[] = L"MicMute";
-	CopyMemory(nid.szTip, _tip, sizeof(_tip) + 1);
+	StringCchCopy(nid.szTip, 128, _tip);
+	StringCchCopy(nid.szInfoTitle, 64, _tip);
+	TCHAR _tooltip_text[] = _T("Started");
+	StringCchCopy(nid.szInfo, 256, _tooltip_text);
 	Shell_NotifyIcon(NIM_ADD, &nid);
 
 	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MIC_MUTE));
+
+	HMENU hmenu = LoadMenu(GetModuleHandle(NULL), MAKEINTRESOURCE(IDC_TRAY_MIC_MUTE));
+	TrayMenu = GetSubMenu(hmenu, 0);
+	SetMenuDefaultItem(TrayMenu, IDM_SHOW_MICMUTE, 0);
+
+	if (StartMuted != 0)
+	{
+		StartMutedToggle(AppHWnd);
+		MuteToggle(AppHWnd);
+	}
 
 	// Main message loop:
 	while (GetMessage(&msg, NULL, 0, 0))
@@ -112,7 +155,18 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 			DispatchMessage(&msg);
 		}
 	}
-	
+
+	_start_muted = (StartMuted == TRUE) ? 1 : 0;
+	GetShortcut(&_count, &_key1, &_key2);
+	_stprintf(_str, _T("%i"), _count);
+	WritePrivateProfileString(_T("Mic_Mute"), _T("ShortCut_Count"), _str, _ini);
+	_stprintf(_str, _T("%i"), _key1);
+	WritePrivateProfileString(_T("Mic_Mute"), _T("ShortCut_Key1"), _str, _ini);
+	_stprintf(_str, _T("%i"), _key2);
+	WritePrivateProfileString(_T("Mic_Mute"), _T("ShortCut_Key2"), _str, _ini);
+	_stprintf(_str, _T("%i"), _start_muted);
+	WritePrivateProfileString(_T("Mic_Mute"), _T("StartMuted"), _str, _ini);
+
 	Shell_NotifyIcon(NIM_DELETE, &nid);
 
 	SetEvent(StopEvent);
@@ -122,9 +176,6 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 	CloseHandle(HookEvent);
 	UnhookWindowsHookEx(hhook);
 	FreeLibrary(hinstDLL);
-
-	mixer_mic_out.SetMute(SavedMicState);
-	mixer_line_out.SetMute(SavedLineState);
 
 	return (int) msg.wParam;
 }
@@ -193,11 +244,21 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    return TRUE;
 }
 
-VOID Mute(HWND hWnd)
+VOID MuteToggle(HWND hWnd)
 {
 	HMENU menu = GetMenu(hWnd);
 	DWORD mute_state;
 	DWORD PrevVolume;
+
+	NOTIFYICONDATA nid;
+	nid.cbSize = sizeof(nid);
+	nid.hWnd = AppHWnd;
+	nid.uID = 1;
+	nid.uFlags = NIF_TIP | NIF_INFO;
+	nid.uTimeout = 5000;
+	nid.dwInfoFlags = NIIF_USER | NIIF_NOSOUND;
+	TCHAR _tooltip_title[] = _T("MicMute");
+	StringCchCopy(nid.szInfoTitle, 64, _tooltip_title);
 
 	FLASHWINFO flash;
 	flash.cbSize = sizeof(flash);
@@ -208,21 +269,27 @@ VOID Mute(HWND hWnd)
 	mute_state = CheckMenuItem(menu, IDM_MUTE, MF_UNCHECKED);
 	if (mute_state == MF_UNCHECKED)
 	{
-		SetWindowText(hWnd, _T("MicMute - Mic Off"));
+		TCHAR _tooltip_text[] = _T("Mic Off");
+		StringCchCopy(nid.szInfo, 265, _tooltip_text);
+		TCHAR _tip[] = _T("MicMute - Mic Off");
+		StringCchCopy(nid.szTip, 128, _tip);
+		SetWindowText(hWnd, (LPTSTR)_tip);
 		CheckMenuItem(menu, IDM_MUTE, mute_state = MF_CHECKED);
 		flash.dwFlags = FLASHW_TRAY;
-		mixer_mic_out.SetMute(TRUE);
-		mixer_line_out.SetMute(TRUE);
 	}
 	else 
 	{
-		SetWindowText(hWnd, _T("MicMute"));
+		TCHAR _tooltip_text[] = _T("Mic On");
+		StringCchCopy(nid.szInfo, 256, _tooltip_text);
+		TCHAR _tip[] = _T("MicMute");
+		StringCchCopy(nid.szTip,128,  _tip);
+		SetWindowText(hWnd, (LPTSTR)_tip);
 		mute_state = MF_UNCHECKED;
 		flash.dwFlags = FLASHW_STOP;
-		mixer_mic_out.SetMute(FALSE);
-		mixer_line_out.SetMute(FALSE);
 	}
+	CheckMenuItem(TrayMenu, IDM_MUTE, mute_state);
 
+	Shell_NotifyIcon(NIM_MODIFY, &nid);
 	FlashWindowEx(&flash);
 
 	PrevVolume = mixer_mic_in.GetVolume();
@@ -238,6 +305,25 @@ VOID Mute(HWND hWnd)
 
 }
 
+VOID StartMutedToggle(HWND hWnd)
+{
+	DWORD mute_state;
+	HMENU menu = GetMenu(hWnd);
+	mute_state = CheckMenuItem(menu, IDM_START_MUTED, MF_UNCHECKED);
+	if (mute_state == MF_UNCHECKED)
+	{
+		CheckMenuItem(menu, IDM_START_MUTED, mute_state = MF_CHECKED);
+		StartMuted = TRUE;
+	}
+	else
+	{
+		mute_state = MF_UNCHECKED;
+		StartMuted = FALSE;
+	}
+	CheckMenuItem(TrayMenu, IDM_START_MUTED, mute_state);	
+}
+
+
 //
 //  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
 //
@@ -248,15 +334,32 @@ VOID Mute(HWND hWnd)
 //  WM_DESTROY	- post a quit message and return
 //
 //
-const TCHAR _help_text1[] = _T("Press \"Right Windows + Control\"");
-const TCHAR _help_text2[] = _T("to mute or unmute microphone");
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	int wmId, wmEvent;
 	PAINTSTRUCT ps;
 	HDC hdc;
+	POINT pt;
 
+	if (message == TrayMsg)
+	{
+		switch ((UINT)lParam)
+		{
+		case WM_LBUTTONDBLCLK:
+			ShowWindow(hWnd, SW_SHOWNORMAL);
+			break;
+		case WM_RBUTTONDOWN:
+			SetForegroundWindow(hWnd);
+			GetCursorPos(&pt);
+			TrackPopupMenu(TrayMenu, 
+				TPM_LEFTALIGN, 
+				pt.x, pt.y, 0, hWnd, NULL); 
+			PostMessage(hWnd, WM_NULL, 0, 0);
+			break;
+		}
+		return 0;
+	}
 	switch (message)
 	{
 	case WM_COMMAND:
@@ -272,25 +375,36 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			DestroyWindow(hWnd);
 			break;
 		case IDM_MUTE:
-			Mute(hWnd);
+			MuteToggle(hWnd);
 			break;
+		case IDM_START_MUTED:
+			StartMutedToggle(hWnd);
+			break;
+		case IDM_SHOW_MICMUTE:
+			ShowWindow(hWnd, SW_SHOWNORMAL);
+			break;
+		case IDM_HIDE_MICMUTE:
+			ShowWindow(hWnd, SW_HIDE);
+			break;
+		case IDM_SETUP_SHORTCUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_SETUP_SHORTCUT), hWnd, SetupShortcut);
 		default:
 			return DefWindowProc(hWnd, message, wParam, lParam);
 		}
+		break;
+	case WM_CLOSE:
+		ShowWindow(hWnd, SW_HIDE);
 		break;
 	case WM_PAINT:
 		hdc = BeginPaint(hWnd, &ps);
 		// TODO: Add any drawing code here...
 
-		hdc = GetDC(hWnd);
-		TextOut(hdc, 5, 5, _help_text1, (sizeof(_help_text1) - 1)/ sizeof(TCHAR));
-		TextOut(hdc, 5, 22, _help_text2, (sizeof(_help_text2) - 1)/ sizeof(TCHAR));
-		ReleaseDC(hWnd, hdc);
-
 		EndPaint(hWnd, &ps);
 		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
+		break;
+	case WM_KEYDOWN:
 		break;
 	default:
 		return DefWindowProc(hWnd, message, wParam, lParam);
@@ -316,4 +430,171 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 		break;
 	}
 	return (INT_PTR)FALSE;
+}
+
+WNDPROC _edit_proc = 0;
+UINT _prev_code;
+INT_PTR CALLBACK SetupShortcut(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		SetEnabled(false);
+		_prev_code = 0;
+		_edit_proc = (WNDPROC)GetWindowLongPtr(GetDlgItem(hDlg, IDC_SHORTCUT), GWLP_WNDPROC);
+		SetWindowLongPtr(GetDlgItem(hDlg, IDC_SHORTCUT), GWLP_WNDPROC, (LONG_PTR)ShortcutEditProc);
+		return (INT_PTR)TRUE;
+	case WM_SHOWWINDOW:
+		SetFocus(GetDlgItem(hDlg, IDC_SHORTCUT));
+		break;
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			SetEnabled(true);
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
+
+TCHAR * KeyToName(UINT _code)
+{
+	static TCHAR _str[32];
+	_str[0] = TCHAR(0);
+	_str[1] = TCHAR(0);
+	switch (_code)
+	{
+	case VK_BACK: return _T("Back"); 
+	case VK_TAB: return _T("Tab");
+	case VK_CLEAR: return _T("Clear");
+	case VK_RETURN: return _T("Return");
+
+	case VK_SHIFT: return _T("Shift");
+	case VK_CONTROL: return _T("Ctrl");
+	case VK_MENU: return _T("Menu");
+	case VK_PAUSE: return _T("Pause");
+	case VK_CAPITAL: return _T("Caps");
+
+	case VK_ESCAPE: return _T("Esc");
+
+	case VK_SPACE: return _T("Space");
+	case VK_PRIOR: return _T("Prior");
+	case VK_NEXT : return _T("Next");
+	case VK_END: return _T("End");
+	case VK_HOME: return _T("Home");
+	case VK_LEFT: return _T("Left");
+	case VK_UP : return _T("Up");
+	case VK_RIGHT: return _T("Right");
+	case VK_DOWN: return _T("Down");
+	case VK_SELECT: return _T("Select");
+	case VK_PRINT: return _T("Print");
+	case VK_EXECUTE: return _T("Exec");
+	case VK_SNAPSHOT: return _T("Snap");
+	case VK_INSERT: return _T("Ins");
+	case VK_DELETE : return _T("Del");
+	case VK_HELP: return _T("Help");
+
+	case VK_LWIN: return _T("Left Win");
+	case VK_RWIN: return _T("Right Win");
+	case VK_APPS : return _T("Apps");
+
+	case VK_SLEEP: return _T("Sleep");
+
+	case VK_NUMPAD0: return _T("Num 0");
+	case VK_NUMPAD1: return _T("Num 1");
+	case VK_NUMPAD2: return _T("Num 2");
+	case VK_NUMPAD3: return _T("Num 3");
+	case VK_NUMPAD4: return _T("Num 4");
+	case VK_NUMPAD5: return _T("Num 5");
+	case VK_NUMPAD6: return _T("Num 6");
+	case VK_NUMPAD7: return _T("Num 7");
+	case VK_NUMPAD8 : return _T("Num 8");
+	case VK_NUMPAD9 : return _T("Num 9");
+	case VK_MULTIPLY : return _T("Num *");
+	case VK_ADD     : return _T("Num +");
+	case VK_SEPARATOR : return _T("Num .");
+	case VK_SUBTRACT: return _T("Num -");
+	case VK_DECIMAL : return _T("Num Dec");
+	case VK_DIVIDE : return _T("Num /");
+	case VK_F1    : return _T("F1");
+	case VK_F2    : return _T("F2");
+	case VK_F3    : return _T("F3");
+	case VK_F4    : return _T("F4");
+	case VK_F5    : return _T("F5");
+	case VK_F6    : return _T("F6");
+	case VK_F7    : return _T("F7");
+	case VK_F8     : return _T("F8");
+	case VK_F9     : return _T("F9");
+	case VK_F10   : return _T("F10");
+	case VK_F11   : return _T("F11");
+	case VK_F12   : return _T("F12");
+	case VK_F13   : return _T("F13");
+	case VK_F14   : return _T("F14");
+	case VK_F15   : return _T("F15");
+	case VK_F16   : return _T("F16");
+	case VK_F17   : return _T("F17");
+	case VK_F18    : return _T("F18");
+	case VK_F19    : return _T("F19");
+	case VK_F20    : return _T("F20");
+	case VK_F21    : return _T("F21");
+	case VK_F22    : return _T("F22");
+	case VK_F23    : return _T("F23");
+	case VK_F24   : return _T("F24");
+
+	case VK_NUMLOCK  : return _T("NumLock");
+	case VK_SCROLL   : return _T("ScrollLock");
+	default: _str[0] = (TCHAR)_code;
+	}
+	return _str;
+}
+
+LRESULT CALLBACK ShortcutEditProc(HWND hEdit, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UINT _code;
+	static TCHAR _str[1024];
+	TCHAR _key1[32], _key2[32];
+	UINT _count;
+	switch (message)
+	{
+	case WM_PAINT:
+		if (_prev_code == 0)
+		{
+			GetShortcut((int *)&_count, (int *)&_prev_code, (int *)&_code);
+			if (_count == 1)
+			{
+				StringCchCopy(_key1, 32, KeyToName(_prev_code));
+				StringCbPrintf(_str, sizeof(_str), _T("%s"), _key1);
+			}
+			else
+			{
+				StringCchCopy(_key1, 32, KeyToName(_prev_code));
+				StringCchCopy(_key2, 32, KeyToName(_code));
+				StringCbPrintf(_str, sizeof(_str), _T("%s + %s"), _key1, _key2);
+			}
+			SetWindowText(hEdit, _str);
+			_prev_code = 0;
+		}
+		break;
+	case WM_KEYDOWN:
+		_code = (UINT)wParam;
+		if ((_prev_code == 0) || (_prev_code == _code))
+		{
+			StringCchCopy(_key1, 32, KeyToName(_code));
+			StringCbPrintf(_str, sizeof(_str), _T("%s"), _key1);
+			SetShortcut(1, _code, 0);
+		}
+		else
+		{
+			StringCchCopy(_key1, 32, KeyToName(_prev_code));
+			StringCchCopy(_key2, 32, KeyToName(_code));
+			StringCbPrintf(_str, sizeof(_str), _T("%s + %s"), _key1, _key2);
+			SetShortcut(2, _prev_code, _code);
+		}
+		SetWindowText(hEdit, _str);
+		_prev_code = _code;
+		break;
+	}
+	return CallWindowProc(_edit_proc, hEdit, message, wParam, lParam);
 }
