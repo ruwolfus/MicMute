57725ac159f914dd5cd1da262364ed686672d1b9
mist.poryvaev <mist.poryvaev@81dcea23-720b-4ff1-bc4e-8d28d5d6887b>
2010-06-28 14:49:11 +0000

    0.1.2

--- trunk/mic_mute/mic_mute/resource.h
+++ trunk/mic_mute/mic_mute/resource.h
@@ -2,7 +2,10 @@
 // Microsoft Visual C++ generated include file.
 // Used by mic_mute.rc
 //
+#define GLOBAL_VERSION_MAJOR            0
+#define GLOBAL_VERSION_MINOR            1
 #define IDC_MYICON                      2
+#define GLOBAL_VERSION_SUB              2
 #define IDD_MIC_MUTE_DIALOG             102
 #define IDS_APP_TITLE                   103
 #define IDD_ABOUTBOX                    103
@@ -11,8 +14,16 @@
 #define IDI_MIC_MUTE                    107
 #define IDI_SMALL                       108
 #define IDC_MIC_MUTE                    109
+#define IDD_SETUP_SHORTCUT              110
+#define IDI_SHORTCUT                    111
 #define IDR_MAINFRAME                   128
 #define IDM_MUTE                        32771
+#define IDC_TRAY_MIC_MUTE               32772
+#define IDM_SHOW_MICMUTE                32773
+#define IDM_HIDE_MICMUTE                32774
+#define IDM_SETUP_SHORTCUT              32775
+#define IDC_SHORTCUT                    32776
+#define IDM_START_MUTED                 32777
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -20,9 +31,9 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        131
-#define _APS_NEXT_COMMAND_VALUE         32774
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32778
+#define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
